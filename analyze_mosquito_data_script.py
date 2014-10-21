0aba3d3b640f688ec8e073fa6b828057b078e3ff
Jens Muhle <jmuhle@ucsd.edu>
2014-10-21 14:27:36 -0700

    Modified script and lib for filename support and automatic processing of all data.csv files

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import analyze_mosquito_data_lib as mosquito_lib
+import sys
+
+filename = sys.argv[1]
 
 #filename = "./rawdata/A1_mosquito_data.csv"
-filename = "A1_mosquito_data.csv"
+#filename = "A1_mosquito_data.csv"
 
-data = pd.read_csv("./rawdata/" + filename)
+data = pd.read_csv(filename)
 data["temperature"] = mosquito_lib.fahr_to_celsius(data["temperature"])
 
-parameters = mosquito_lib.analyze(data, "plot.png")
+parameters = mosquito_lib.analyze(data, filename.replace("csv","png"))
 
 # Save parameters to file
-parameters.to_csv("parameters.csv")
+parameters.to_csv(filename.replace("data","parameters"))
 
 print data.head()
